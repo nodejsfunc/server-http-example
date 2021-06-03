d96255bd179efba27254e1b8c7bf4a80555470c4
tom <tom@sapletta.com>
2021-06-03 18:16:54 +0200

    update .apicra

---
+++ .apicra/docs.bat
@@ -0,0 +1,5 @@
+set URL=https://github.com/paas-info/www
+::set BROWSER=chrome
+set BROWSER=firefox
+start %BROWSER% %URL%
+::explorer %URL%
---
+++ .apicra/remote_update.bat
@@ -0,0 +1,3 @@
+cd F:\PycharmProjects\faas-ovh-api\
+py deploy.py app.goethe.pl update.sh
+cd F:\WebstormProjects\goethe-pl-app
